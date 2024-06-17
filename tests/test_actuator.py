b2ea32e67ea201afc8961612193848caa11f9eaf
riley206 <rileymaltos@gmail.com>
2024-06-17 12:52:31 -0700

    Added more tests

--- tests/test_actuator.py
+++ tests/test_actuator.py
@@ -26,6 +26,18 @@ class TestNewTask:
         reservation_manager._cleanup = MagicMock()
         reservation_manager.save_state = MagicMock()
         return reservation_manager
+    @pytest.fixture
+    def PDA(self):
+        parent = Mock()
+        parent.vip = Mock()
+        parent.vip.config.get = MagicMock(return_value=pickle.dumps({}))
+        parent.vip.config.set = MagicMock()
+        parent.config = Mock()
+        parent.config.reservation_publish_interval = 60  # Mock the interval for testing
+        grace_time = 10
+
+        PDA = PlatformDriverAgent()
+        return PDA
     def test_new_task_valid_inputs(self, setup):
         result = setup.new_task(self.sender, self.task_id, priority='HIGH', requests=self.requests)
         assert result.success
@@ -37,7 +49,6 @@ class TestNewTask:
         assert result.info_string == 'MISSING_AGENT_ID' and not result.success
     def test_invalid_task_id(self, setup):
         """ Tests task request with missing task id, empty task id, and int task id"""
-
         result = setup.new_task(self.sender, task_id=None, priority="HIGH", requests=self.requests)
         assert result.info_string == 'MISSING_TASK_ID' and not result.success
 
@@ -47,6 +58,7 @@ class TestNewTask:
         result = setup.new_task(self.sender, task_id=1234, priority="HIGH", requests=self.requests)
         assert result.info_string == 'MALFORMED_REQUEST: TypeError: taskid must be a nonempty string' and not result.success
     def test_requests_malformed(self, setup):
+        """ Tests malformed request by creating new task with empty dates"""
         result = setup.new_task(self.sender, self.task_id, priority="HIGH", requests=[])
         assert result.info_string == 'MALFORMED_REQUEST_EMPTY' and not result.success
     def test_new_task_missing_priority(self, setup):
@@ -56,6 +68,7 @@ class TestNewTask:
         result = setup.new_task(self.sender, self.task_id, priority="low", requests=self.requests)
         assert result.success
     def test_invalid_priority(self, setup):
+        """ Tests an invalid priority (Medium priority does not exist)"""
         result = setup.new_task(self.sender, self.task_id, priority="MEDIUM", requests=self.requests)
         assert result.info_string == 'INVALID_PRIORITY' and not result.success
     def test_task_exists(self, setup):
@@ -68,13 +81,86 @@ class TestNewTask:
         assert result.info_string == 'TASK_ID_ALREADY_EXISTS' and result.success == False
     def test_request_new_task_should_succeed_on_preempt_self(self, setup):
         """
-        Test schedule preemption by a higher priority task from the same agent.
+        Test schedule preemption by a higher priority task from the same sender.
         """
         result = setup.new_task(self.sender, self.task_id, priority='LOW_PREEMPT', requests=self.requests)
         assert result.success
         result = setup.new_task(self.sender, "high_priority_task_id", priority='HIGH', requests=self.requests)
         assert result.success
         assert result.info_string == 'TASKS_WERE_PREEMPTED'
+    def test_schedule_preempt_other(self, setup):
+        """
+        Test schedule preemption by a higher priority task from a different sender.
+        """
+        result = setup.new_task("agent1", self.task_id, priority='LOW_PREEMPT', requests=self.requests)
+        assert result.success
+        result = setup.new_task("agent2", "high_priority_task_id", priority='HIGH', requests=self.requests)
+        assert result.success
+        assert result.info_string == 'TASKS_WERE_PREEMPTED'
+    def test_reservation_conflict(self, setup):
+        """
+        Test task conflict from different agents.
+        """
+        result = setup.new_task("agent1", self.task_id, priority='LOW', requests=self.requests)
+        assert result.success
+        result = setup.new_task("agent2", "different_task_id", priority='LOW', requests=self.requests)
+        assert result.info_string == 'CONFLICTS_WITH_EXISTING_RESERVATIONS'
+    def test_reservation_conflict_self(self, setup):
+        """
+        Test task conflict from one request.
+        """
+        # two tasks with same time frame
+        requests = [
+            ['device1', '2022-01-01T00:00:00', '2022-01-01T01:00:00'],
+            ['device1', '2022-01-01T00:00:00', '2022-01-01T01:00:00']
+        ]
+        result = setup.new_task("agent2", self.task_id, priority='LOW', requests=requests)
+        assert result.info_string == 'REQUEST_CONFLICTS_WITH_SELF'
+    def test_schedule_overlap(self, setup):
+        """
+        Test successful task when end time of one time slot is the same as
+        start time of another slot.
+        """
+        time_1 = ['device1', '2022-01-01T00:00:00', '2022-01-01T01:00:00']
+        time_2 = ['device2', '2022-01-01T01:00:00', '2022-01-02T01:00:00']
+        result = setup.new_task("agent1", self.task_id, priority='LOW', requests=time_1)
+        assert result.success
+        result = setup.new_task("agent2", "different_task_id", priority='LOW', requests=time_2)
+        assert result.success
+    def test_cancel_error_invalid_task(self, setup):
+        """
+        Test invalid task id when trying to cancel a task.
+        """
+        # creating task with a task_id of "task_that_exists"
+        result = setup.new_task(self.sender, task_id="task_that_exists", priority='LOW', requests=self.requests)
+        assert result.success
+        # trying to cancel a task with a task_id of "unexistent_task_id"
+        result = setup.cancel_task(sender=self.sender, task_id="unexistent_task_id")
+        assert result.info_string == 'TASK_ID_DOES_NOT_EXIST'
+
+    def test_get_point(self, PDA):
+        PDA = PlatformDriverAgent()
+
+        # Mock the remote object and its get_point method
+        mock_remote = MagicMock()
+        mock_remote.get_point.return_value = 20.5
+
+        # Mock the device node
+        mock_node = MagicMock()
+        mock_node.get_remote.return_value = mock_remote
+
+        # Mock the _equipment_id method
+        PDA._equipment_id = MagicMock()
+        PDA._equipment_id.return_value = 'fakedriver1/SampleWritableFloat1'
+
+        # Mock the get_node method
+        PDA.equipment_tree.get_node = MagicMock()
+        PDA.equipment_tree.get_node.return_value = mock_node
+
+        result = PDA.get_point('fakedriver1', 'SampleWritableFloat1')
+
+        assert result == 20.5
+
 class TestCancelTask:
     sender = "test.agent"
     task_id = "test_task_id"
