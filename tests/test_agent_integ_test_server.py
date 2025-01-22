ce7c1281da74b6e81b4602aea9d84c46581cc1c4
riley206 <rileymaltos@gmail.com>
2025-01-22 14:17:16 -0800

    converted more tests to use test server

--- tests/test_agent_integ_test_server.py
+++ tests/test_agent_integ_test_server.py
@@ -23,7 +23,7 @@
 # }}}
 
 import json
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, ANY
 from platform_driver.agent import PlatformDriverAgent
 from platform_driver.overrides import OverrideManager
 from platform_driver.reservations import ReservationManager
@@ -49,6 +49,7 @@ def return_config(pattern):
 def test_instantiate():
     ts = TestServer()
     pda = ts.instantiate_agent(PlatformDriverAgent)
+    # om = OverrideManager(pda)
 
     driver_config = {
         "driver_type": "fake",
@@ -93,6 +94,16 @@ def test_instantiate():
         "Starting Value": "10",
         "Type": "float",
         "Notes": "Setpoint to enable demand control ventilation"
+    },
+    {
+        "Point Name": "SampleWritableFloat2",
+        "Volttron Point Name": "SampleWritableFloat2",
+        "Units": "PPM",
+        "Units Details": "1000.00 (default)",
+        "Writable": True,
+        "Starting Value": "10",
+        "Type": "float",
+        "Notes": "Setpoint to enable demand control ventilation"
     }
 ],
         "interval": 5,
@@ -113,20 +124,122 @@ def test_instantiate():
     pda.override_manager = OverrideManager(pda)
     now = get_aware_utc_now()
     pda.reservation_manager = ReservationManager(pda, pda.config.reservation_preempt_grace_time, now)
-    pda._configure_new_equipment('devices/campus/building/fake', 'NEW', FakeConfig, schedule_now=False)
+    pda._configure_new_equipment('devices/campus/building/fake', 'NEW', FakeConfig, schedule_now=True)
     pda.vip = MagicMock()
     pda.vip.rpc.context = MagicMock()
     pda.vip.rpc.context.vip_message.peer = 'some.caller'
 
+    #####################
+    # Get Point / Set Point
+    #####################
     result = pda.get_point('devices/campus/building/fake/SampleWritableFloat1')
     print(f"result: {result}")
     assert result == 10
     result = pda.set_point('devices/campus/building/fake', 'SampleWritableFloat1', 15)
     assert result == 15
+    result = pda.get_point('devices/campus/building/fake/SampleWritableFloat1')
+    assert result == 15
 
+    #####################
+    # Test get multiple points / set multiple points
+    #####################
+    pda.set_multiple_points(
+        'devices/campus/building/fake',
+        [
+            ("SampleWritableFloat1", 1),
+            ("SampleWritableFloat2", 1),
+            ("OutsideAirTemperature1", 1)  # Should not change this
+        ]
+    )
+
+    pda.set_multiple_points(
+        'devices/campus/building/fake',
+        [
+            ("SampleWritableFloat1", 15),
+            ("SampleWritableFloat2", 15),
+            ("OutsideAirTemperature1", 100)  # Should not change this
+        ]
+    )
 
     result = pda.get_multiple_points(['devices/campus/building/fake/SampleWritableFloat1',
+                                      'devices/campus/building/fake/SampleWritableFloat2',
                                       'devices/campus/building/fake/OutsideAirTemperature1'])
     assert result == ({'devices/campus/building/fake/OutsideAirTemperature1': 50.0,
-                       'devices/campus/building/fake/SampleWritableFloat1': 15.0}, {})
+                       'devices/campus/building/fake/SampleWritableFloat1': 15.0,
+                       'devices/campus/building/fake/SampleWritableFloat2': 15}, {})
+
+    #####################
+    # Test reverting a point (not working)
+    #####################
+    # Set TestPoint1 to 50.0
+    # result = pda.set_point('devices/campus/building/fake', 'SampleWritableFloat2', 50)
+    # assert result == 50
+    # pda.revert_point('devices/campus/building/fake', 'SampleWritableFloat2')
+    # result = pda.get_point('devices/campus/building/fake/SampleWritableFloat2')
+    # assert result == 10
+
+    #####################
+    # Override on and off
+    #####################
+    pda.core.spawn = MagicMock()
+    pda.set_override_on("devices/campus/building/fake")
+    assert "devices/campus/building/fake" in pda.get_override_devices()
+    pda.set_override_off("devices/campus/building/fake")
+    assert "devices/campus/building/fake" not in pda.get_override_devices()
+
+    #####################
+    # Check Schedule
+    #####################
+    schedule = pda.get_poll_schedule()
+    # schedule = ba.vip.rpc.call("platform.driver", "get_poll_schedule").get(timeout=10)
+    assert schedule, "Poll schedule should not be empty."
+    assert "default" in schedule, "Default polling group should exist."
+
+    #####################
+    # Test scrape_all
+    #####################
+    result = pda.scrape_all('devices/campus/building/fake')
+    expected_result = ({'devices/campus/building/fake/EKG': ANY,
+      'devices/campus/building/fake/Heartbeat': True,
+      'devices/campus/building/fake/OutsideAirTemperature1': 50.0,
+      'devices/campus/building/fake/SampleWritableFloat1': 15.0,
+      'devices/campus/building/fake/SampleWritableFloat2': 15.0},
+     {})
+    assert result == expected_result
+
+    # def test_enable_disable_device(driver_setup):
+    #     #TODO disable it then read the configuration.
+    #     vi, ba, device_name = driver_setup
+    #
+    #     # Disable the device
+    #     ba.vip.rpc.call("platform.driver", "disable", f"devices/{device_name}").get(timeout=10)
+    #
+    #
+    #     result = ba.vip.rpc.call("platform.driver", "get_point", f"devices/{device_name}", "TestPoint1").get(timeout=10)
+    #     assert result == "something"
+    #
+    #     # Re-enable the device
+    #     ba.vip.rpc.call("platform.driver", "enable", f"devices/{device_name}").get(timeout=10)
+    #
+    #     # Should work again
+    #     val = ba.vip.rpc.call("platform.driver", "get_point", f"devices/{device_name}", "TestPoint1").get(timeout=10)
+    #     assert val is not None, "After enabling the device, we should be able to read points."
+
+    # def test_start_stop_points(driver_setup):
+    #     #TODO make sure it does not do a new reading? check for an error?
+    #     vi, ba, device_name = driver_setup
+    #
+    #     # Stop all points on device
+    #     ba.vip.rpc.call("platform.driver", "stop", f"devices/{device_name}").get(timeout=10)
+    #
+    #     # Attempt to get_point might still succeed or raise an error,
+    #     # depending on how your code handles stopped points,
+    #     # but they won't be polled in the background.
+    #
+    #     # Start them again
+    #     ba.vip.rpc.call("platform.driver", "start", f"devices/{device_name}").get(timeout=10)
+    #
+    #     # Confirm we can read them after they've started again
+    #     val = ba.vip.rpc.call("platform.driver", "get_point", f"devices/{device_name}", "TestPoint2").get(timeout=10)
+    #     assert val is not None, "Should be able to read TestPoint2 after start() was called."
 
